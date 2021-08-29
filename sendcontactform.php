7af32c03f6298437e029a0cb8e07b22949ea466d
MadRoosterCro <dario.car.kz@gmail.com>
2021-08-29 12:28:39 +0200

    first commit

---
+++ sendcontactform.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "dario.car.kz@gmail.com";
+$subject = $_POST['subject'];
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You for the inquiry, I'll get back to you ASAP!";
+?>
